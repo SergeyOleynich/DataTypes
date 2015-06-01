2c6f56f1bccc01e4f29efad2fc5113710fbfd41b
Sergey <for_sale@I-hate-you-guys.local>
2015-06-01 19:36:04 +0300

    Initial Commit

---
+++ SODataTypes(lesson 6)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SODataTypes(lesson 6)
+//
+//  Created by Sergey on 01.06.15.
+//  Copyright (c) 2015 Sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SODataTypes(lesson 6)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SODataTypes(lesson 6)
+//
+//  Created by Sergey on 01.06.15.
+//  Copyright (c) 2015 Sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
